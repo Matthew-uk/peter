6c7e7603767280ffbaf499471161eb80d302cfbd
Peter Njoku <iwezupeter833@gmail.com>
2025-10-23 20:08:26 +0200

    created a fucntion and connected de botton to a event

--- js/main.js
+++ js/main.js
@@ -4,9 +4,12 @@ const Password=document.getElementById("Password")
 const Button=document.getElementById("button")
 
 // connect the button to the function
+Button.addEventListener("click", submitForm)
 
 //function we want to run whenever we submit the form
 function submitForm(e) {
     e.preventDefault();
     console.log(Firstname.value)
+    console.log(Lastname.value)
+    console.log(Password.value)
 }
