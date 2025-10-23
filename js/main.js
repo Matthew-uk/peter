8909750f2ac113b1fffad8e76e63f97181ecdcb1
Matthew-uk <esaduviedede@gmail.com>
2025-10-23 19:01:34 +0100

    I added a function for the button submit so I want you to connect the button to the function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ const Firstname=document.getElementById("Fname")
 const Lastname=document.getElementById("Lname")
 const Password=document.getElementById("Password")
 const Button=document.getElementById("button")
+
+// connect the button to the function
+
+//function we want to run whenever we submit the form
+function submitForm(e) {
+    e.preventDefault();
+    console.log(Firstname.value)
+}
