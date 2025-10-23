2614a47a9286882b62c32d61ab159774f3d69df9
Peter Njoku <iwezupeter833@gmail.com>
2025-10-23 19:48:24 +0200

    I added id to all the variables and called them out in the js file

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const Firstname=document.getElementById("Fname")
+const Lastname=document.getElementById("Lname")
+const Password=document.getElementById("Password")
+const Button=document.getElementById("button")
