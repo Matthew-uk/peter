99f8b4030bdecd9a2b487bedee85010038a47aad
Peter Njoku <iwezupeter833@gmail.com>
2025-10-29 19:40:57 +0100

    Created a new form with the same divs as the last one but change the function, now it multiplies

--- js/multiplication.js
+++ js/multiplication.js
@@ -10,3 +10,16 @@ function multplication(e){
     console.log(NumberA.value*NumberB.value)
 }
 Button.addEventListener("click",multplication)
+
+const NumberC=document.getElementById("numberC")
+const NumberD=document.getElementById("numberD")
+const ButtonB=document.getElementById("buttonb")
+const P_element=document.getElementById("result")
+
+function division(w){
+    w.preventDefault()
+    console.log(NumberC.value)
+    console.log(NumberD.value)
+    console.log(NumberC.value/NumberD.value)
+}
+Button.addEventListener("click",division)
