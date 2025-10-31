71ecc4ce5e8bb08c3bfaac0d8567a1510c109254
Matthew-uk <esaduviedede@gmail.com>
2025-10-31 18:55:36 +0100

    Added functionality to web app

--- js/list.js
+++ js/list.js
@@ -2,10 +2,10 @@ const button = document.getElementById('button');
 const list = document.getElementById('list'); // List is our input.
 const ul = document.getElementById('ul');
 
-var groceries = [];
+var groceries = []; // Here we created an empty groceries array
 
 function addGroceries() {
-  groceries.push(list.value);
+  groceries.push(list.value); // groceries.push add an item to the groceries array
   console.log(groceries);
 }
 
